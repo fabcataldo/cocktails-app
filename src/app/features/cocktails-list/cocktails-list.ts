a4c36d31cb64f1e4967a5a4ab09eae85aff48945
Fabio <fabioalbertocataldo@gmail.com>
2025-09-24 17:42:02 -0300

    add: CocktailsListPageFacade provider added to CocktailsList

--- src/app/features/cocktails-list/cocktails-list.ts
+++ src/app/features/cocktails-list/cocktails-list.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { CocktailsListPageFacade } from './services/cocktails-list-page-facade';
 
 @Component({
   selector: 'app-cocktails-list',
   imports: [],
   templateUrl: './cocktails-list.html',
-  styleUrl: './cocktails-list.scss'
+  styleUrl: './cocktails-list.scss',
+  providers: [CocktailsListPageFacade]
 })
 export class CocktailsList {
 
