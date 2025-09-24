3e2013ec55d9bd0af27cf8d7405cacda47995a6a
Fabio <fabioalbertocataldo@gmail.com>
2025-09-24 17:40:16 -0300

    refactor: alcoholics facade state management

--- src/app/data-access/states/alcoholics/alcoholics.facade.ts
+++ src/app/data-access/states/alcoholics/alcoholics.facade.ts
@@ -1,25 +1,18 @@
-import { Injectable } from '@angular/core'; 
+import { inject, Injectable } from '@angular/core'; 
 import { select, Store } from '@ngrx/store'; 
 import * as actions from './alcoholics.actions';
 import * as selectors from './alcoholics.selectors';
-import { Observable } from 'rxjs';
-import { filter } from '../../interfaces/filter.interface';
 
 @Injectable() 
-export class AlcoholicsFacade {  
-  public alcoholicsFilterItems$: Observable<filter[] | null> | null = null;
-  public loading$: Observable<boolean> | null = null;
-  public loaded$: Observable<boolean> | null = null;
-  public error$: Observable<any> | null = null;
+export class AlcoholicsFacade {
+  private readonly store = inject(Store);
+
+  public alcoholicsFilterItems$ = this.store.pipe(select(selectors.getAlcoholicsFilterItems));
+  public loading$ = this.store.pipe(select(selectors.getLoading));
+  public loaded$ = this.store.pipe(select(selectors.getLoaded));
+  public error$ = this.store.pipe(select(selectors.getError));
  
-  constructor(private readonly store: Store) {}  
-
-  init() {     
-    this.alcoholicsFilterItems$ = this.store.pipe(select(selectors.getAlcoholicsFilterItems));
-    this.loading$ = this.store.pipe(select(selectors.getLoading));
-    this.loaded$ = this.store.pipe(select(selectors.getLoaded));
-    this.error$ = this.store.pipe(select(selectors.getError));
-
+  init() {
     this.store.dispatch(actions.init());  
   }
 
