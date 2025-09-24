bd1836a265ef00dad436d991996974db7f525810
Fabio <fabioalbertocataldo@gmail.com>
2025-09-24 16:26:29 -0300

    add: initial components

---
+++ src/app/features/cocktail-detail/cocktail-detail.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cocktail-detail',
+  imports: [],
+  templateUrl: './cocktail-detail.html',
+  styleUrl: './cocktail-detail.scss'
+})
+export class CocktailDetail {
+
+}
