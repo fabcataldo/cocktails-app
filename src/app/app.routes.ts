1146cf52119bb2e7ed77c58d389fc564a4fe5757
Fabio <fabioalbertocataldo@gmail.com>
2025-09-24 16:24:31 -0300

    add: app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { CocktailsList } from './features/cocktails-list/cocktails-list';
+import { CocktailDetail } from './features/cocktail-detail/cocktail-detail';
+import { NotFound } from './features/not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: CocktailsList },
+    { path: 'detail/:id', component: CocktailDetail},
+    { path: 'not-found', component: NotFound },
+    { path: '**', redirectTo: '/not-found' },
+];
