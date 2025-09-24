89c51f4f1f8d9a8c379b3a5654b1a36b980bf761
Fabio <fabioalbertocataldo@gmail.com>
2025-09-24 17:41:09 -0300

    add: different stores state management in app.config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -9,6 +9,20 @@ import { provideEffects } from '@ngrx/effects';
 import { AlcoholicsEffects } from './data-access/states/alcoholics/alcoholics.effects';
 import { provideStoreDevtools } from '@ngrx/store-devtools'; 
 import { AlcoholicsFacade } from './data-access/states/alcoholics/alcoholics.facade';
+import { categoriesReducer } from './data-access/states/categories/categories.reducer';
+import { glassesReducer } from './data-access/states/glasses/glasses.reducer';
+import { ingredientsReducer } from './data-access/states/ingredients/ingredients.reducer';
+import { CategoriesEffects } from './data-access/states/categories/categories.effects';
+import { GlassesEffects } from './data-access/states/glasses/glasses.effects';
+import { IngredientsEffects } from './data-access/states/ingredients/ingredients.effects';
+import { cocktailsReducer } from './data-access/states/cocktails/cocktails.reducer';
+import { CocktailsEffects } from './data-access/states/cocktails/cocktails.effects';
+import { CategoriesFacade } from './data-access/states/categories/categories.facade';
+import { GlassesFacade } from './data-access/states/glasses/glasses.facade';
+import { IngredientsFacade } from './data-access/states/ingredients/ingredients.facade';
+import { CocktailsFacade } from './data-access/states/cocktails/cocktails.facade';
+import { CocktailEffects } from './data-access/states/cocktail/cocktail.effects';
+import { CocktailFacade } from './data-access/states/cocktail/cocktail.facade';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -17,13 +31,27 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
 
     provideStore({
-      alcoholicsFilterItems: alcoholicsReducer
+      alcoholicsFilterItems: alcoholicsReducer,
+      categoriesFilterItems: categoriesReducer,
+      glassesFilterItems: glassesReducer,
+      ingredientsFilterItems: ingredientsReducer,
+      cocktailsReducer: cocktailsReducer
     }),
     provideEffects([
       AlcoholicsEffects,
+      CategoriesEffects,
+      GlassesEffects,
+      IngredientsEffects,
+      CocktailsEffects,
+      CocktailEffects
     ]),
     (isDevMode() ? [provideStoreDevtools({ maxAge: 25 })] : []),
 
-    AlcoholicsFacade
+    AlcoholicsFacade,
+    CategoriesFacade,
+    GlassesFacade,
+    IngredientsFacade,
+    CocktailsFacade,
+    CocktailFacade
   ],
 };
