38aa2a162a3832757d4fe5224bc09e36c1c39bb0
Fabio <fabioalbertocataldo@gmail.com>
2025-09-25 13:28:55 -0300

    add: darkmode

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ToggleSwitchModule } from 'primeng/toggleswitch';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ToggleSwitchModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
+  isDarkModeSetted = signal<boolean>(false);
+  iconClass = computed(() => 
+    `!text-xs pi ${this.isDarkModeSetted() ? 'pi-moon' : ''}`
+  );
 
+  changeToDarkMode(event: any) {
+    document.body.classList.toggle('dark-theme');
+  }
 }
